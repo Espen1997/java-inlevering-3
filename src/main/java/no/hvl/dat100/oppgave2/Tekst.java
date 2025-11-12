e4b84f705052c787c00367782b412fd299845041
Espen1997 <eslau1045@hvl.no>
2025-11-12 22:40:29 +0100

    Oppgave 1 og 2

--- src/main/java/no/hvl/dat100/oppgave2/Tekst.java
+++ src/main/java/no/hvl/dat100/oppgave2/Tekst.java
@@ -5,39 +5,43 @@ import no.hvl.dat100.oppgave1.*;
 
 public class Tekst extends Innlegg {
 
-	// TODO: objektvariable 
-	
-	public Tekst () {
-		
-	}
-	
-	public Tekst(int id, String bruker, String dato, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
-	}
-	
-	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
-	}
-	
-	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
+    private String tekst;
 
-	}
+    public Tekst() {
+        super(); // Kall konstruktøren til superklassen
+    }
 
-	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    public Tekst(int id, String bruker, String dato, String tekst) {
+        super(id, bruker, dato); // Kall konstruktøren til superklassen
+        this.tekst = tekst;
+    }
 
-	@Override
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+    public Tekst(int id, String bruker, String dato, int likes, String tekst) {
+        super(id, bruker, dato, likes); // Kall konstruktøren til superklassen
+        this.tekst = tekst;
+    }
 
-	}
-	
-	// Metoden nedenfor er kun for valgfri oppgave 6
-	public String toHTML() {
-			
-		throw new UnsupportedOperationException(TODO.method());
-				
-	}
+    public String getTekst() {
+        return tekst;
+    }
+
+    public void setTekst(String tekst) {
+        this.tekst = tekst;
+    }
+
+    @Override
+    public String toString() {
+        return "TEKST\n" +
+                getId() + "\n" +
+                getBruker() + "\n" +
+                getDato() + "\n" +
+                getLikes() + "\n" +
+                tekst + "\n";
+    }
+
+    // Metoden nedenfor er kun for valgfri oppgave 6
+    public String toHTML() {
+        return null; // Implementer denne hvis du vil ha HTML-utskrift
+    }
 }
+
